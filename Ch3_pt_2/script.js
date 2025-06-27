24639370b3d8857d24dd3218fa9a6039ea156ee9
Gana_023 <105679129+Wang023@users.noreply.github.com>
2025-06-27 17:24:56 -0500

    Add files via upload

---
+++ Ch3_pt_2/script.js
@@ -0,0 +1,30 @@
+// Gana Sehaki 06/27/2025
+
+// variable to store my full name
+let fullName = "Gana Sehaki";
+console.log("Full Name:", fullName);
+
+// variable to store my desired annual salary
+let desiredAnnualSalary = 120000;
+console.log("Desired Annual Salary:", desiredAnnualSalary);
+
+// variable to store my veteran status (boolean)
+let veteranStatus = false;
+console.log("Veteran Status:", veteranStatus);
+
+// array to store the names of three of my friends
+let friendsNames = ["Nacer", "Juba", "Sonia"];
+console.log("Friends Names:", friendsNames);
+
+// array to store the desired annual salary of my three friends
+let friendsDesiredSalaries = [100000, 1150000, 105000];
+console.log("Friends Salaries:", friendsDesiredSalaries);
+
+// literal object to store the first name, last name, and desired salary of another friend
+let anotherFriend = {
+  firstName: "Karim",
+  lastName: "Sellami",
+  desiredSalary: 92000
+};
+console.log("Another Friend :", anotherFriend);
+
